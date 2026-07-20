598b0eadce20f66da9693865f8de36721322fac1
MrSn001 <shaheen36.saa@gmail.com>
2026-07-20 11:56:35 +0400

    Implemented the Enums Class

---
+++ ClothingStoreProject/ClothingStore/Enums.cs
@@ -0,0 +1,30 @@
+﻿namespace ClothingStore
+{
+    public class Enums
+    {
+
+        public enum Gender
+        {
+            Men,
+            Women,
+            Unisex,
+            Kids
+        }
+
+        public enum ClothingStyle
+        {
+            Casual,
+            Formal,
+            Sportswear,
+            Streetwear,
+            Vintage
+        }
+
+        public enum Season
+        {
+            SpringSummer,
+            FallWinter,
+            AllSeason
+        }
+    }
+}
