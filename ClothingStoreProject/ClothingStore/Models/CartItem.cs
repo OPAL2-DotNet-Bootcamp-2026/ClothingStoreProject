c6860462690b2565022b842f420310ce8855cb37
Ahmed <alshukairi1234@gmail.com>
2026-07-21 10:50:28 +0400

    finished the set up for cartItem class

--- ClothingStoreProject/ClothingStore/Models/CartItem.cs
+++ ClothingStoreProject/ClothingStore/Models/CartItem.cs
@@ -2,5 +2,10 @@
 {
     public class CartItem
     {
+        public int cartItemId { get; set; }
+        public int cartId { get; set; }
+        public int variantId { get; set; }
+        public int quantity { get; set; }
+        public DateTime addedAt { get; set; }
     }
 }
