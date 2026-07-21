deb2e7c7556bff39ab4317e0aef608eac20824e7
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-21 09:27:25 +0400

    Add Error Message and Source Tag Legend  in review class

--- ClothingStoreProject/ClothingStore/Models/Review.cs
+++ ClothingStoreProject/ClothingStore/Models/Review.cs
@@ -7,25 +7,32 @@ namespace ClothingStore.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int reviewId { get; set; }
+        public int reviewId { get; set; }//system generated 
 
-        [Required]
-        [Range(1, 5)]
-        public int rating { get; set; }
 
-        [MaxLength(1000)]
-        public string? comment { get; set; }
-        [Required]
-        public DateTime reviewDate { get; set; }
+        [Required(ErrorMessage = "Rating is required.")]
+        [Range(1, 5,ErrorMessage = "Rating must be between 1 and 5.")]
+        public int rating { get; set; }//user input — 1 to 5 stars
+
+
+        [MaxLength(1000,ErrorMessage = "Comment cannot exceed 1000 characters.")]
+        public string? comment { get; set; }//user input 
+
+
+        [Required(ErrorMessage = "Review date is required.")]
+        public DateTime reviewDate { get; set; } // system generated — set to today's date
 
 
         [ForeignKey("user")]//foreign  key to user 
-        public int userId { get; set; }
-        public User user { get; set; }
+        [Required(ErrorMessage = "User ID is required.")]
+        public int userId { get; set; }//from the list — from logged-in user
+        public User user { get; set; }// navigation property ,(user & review)
+
 
 
         [ForeignKey("product")]//foreign  key to product
-        public int productId { get; set; }
-        public Product product { get; set; }
+        [Required(ErrorMessage = "Product ID is required.")]
+        public int productId { get; set; }//from the list  — chosen from purchased products
+        public Product product { get; set; }//navigation property ,(product & review)
     }
 }
