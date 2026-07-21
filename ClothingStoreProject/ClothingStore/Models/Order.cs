249e61feb5c519f22581e626e6c8103d127d31fd
Ahmed <alshukairi1234@gmail.com>
2026-07-21 12:21:40 +0400

    finished setting up the order class

--- ClothingStoreProject/ClothingStore/Models/Order.cs
+++ ClothingStoreProject/ClothingStore/Models/Order.cs
@@ -2,13 +2,30 @@
 {
     public class Order
     {
-        public int orderId {get; set;}
-        public int userId {get; set;}
-        public DateTime  orderDate {get; set;}
-        public decimal totalPrice {get; set;}
-        public string status = "pendeng" {get; set;}
-        public string shippingAddress {get; set;}
-        
-        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int orderId { get; set; }
+
+        [Required]
+        public int userId { get; set; }
+
+        [Required]
+        public DateTime orderDate { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0, double.MaxValue)]
+        public decimal totalAmount { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string status { get; set; } = "Pending";
+
+        [Required]
+        [MaxLength(300)]
+        public string shippingAddress { get; set; }
+
+        public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
     }
+}
 }
