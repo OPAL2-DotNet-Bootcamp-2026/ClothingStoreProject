5618d77d1a30ada719929266fecf9cff7bfb0a80
Ahmed <alshukairi1234@gmail.com>
2026-07-21 10:46:19 +0400

    finished the set up for cart class

--- ClothingStoreProject/ClothingStore/Models/Cart.cs
+++ ClothingStoreProject/ClothingStore/Models/Cart.cs
@@ -2,5 +2,9 @@
 {
     public class Cart
     {
+        public int carId { get; set; }  
+        public int userId {get; set;} 
+        public DateTime createdAt {get; set;}
+        public DateTime  updatedAt {get; set;}
     }
 }
