9b7aadaae501222ba686da72fbe4560c5d22fbad
Ahmed <alshukairi1234@gmail.com>
2026-07-21 12:26:25 +0400

    feat- fineshed setting up the cartItem class

--- ClothingStoreProject/ClothingStore/Models/CartItem.cs
+++ ClothingStoreProject/ClothingStore/Models/CartItem.cs
@@ -5,10 +5,28 @@ namespace ClothingStore.Models
 {
     public class CartItem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int cartItemId { get; set; }
+
+        [Required]
+        [ForeignKey("Cart")]
         public int cartId { get; set; }
+
+        [Required]
+        [ForeignKey("ProductVariant")]
         public int variantId { get; set; }
+
+        [Required]
+        [Range(1, 999)]
         public int quantity { get; set; }
-        public DateTime addedAt { get; set; }
+
+        [Required]
+        public DateTime addedAt { get; set; } = DateTime.UtcNow;
+
+        public virtual Cart Cart { get; set; }
+        
+        // Uncomment once the ProductVariant model is set up in your context
+        // public virtual ProductVariant ProductVariant { get; set; }
     }
 }
