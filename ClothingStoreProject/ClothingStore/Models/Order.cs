949df12da90364fefe2363f3a4b3385b5fc1dd34
Ahmed <alshukairi1234@gmail.com>
2026-07-21 11:42:22 +0400

    finish the set up of order class

--- ClothingStoreProject/ClothingStore/Models/Order.cs
+++ ClothingStoreProject/ClothingStore/Models/Order.cs
@@ -2,5 +2,12 @@
 {
     public class Order
     {
+        public int orderId {get; set;}
+        public int userId {get; set;}
+        public DateTime  orderDate {get; set;}
+        public decimal totalPrice {get; set;}
+        public string status = "pendeng" {get; set;}
+        public string shippingAddress {get; set;}
+        
     }
 }
