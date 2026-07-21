e009b4494f81f7218e9c97e66e4890dabb041949
Ahmed <alshukairi1234@gmail.com>
2026-07-21 12:29:25 +0400

    feat- finished setting up cart class

--- ClothingStoreProject/ClothingStore/Models/Cart.cs
+++ ClothingStoreProject/ClothingStore/Models/Cart.cs
@@ -6,10 +6,20 @@ namespace ClothingStore.Models
 {
     public class Cart
     {
-        public int carId { get; set; }  
-        public int userId {get; set;} 
-        public DateTime createdAt {get; set;}
-        public DateTime  updatedAt {get; set;}
-        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int cartId { get; set; }
+
+        [Required]
+        public int userId { get; set; }
+
+        // Keeping one active cart per user as per specs
+        public virtual ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+        [Required]
+        public DateTime createdAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime updatedAt { get; set; } = DateTime.UtcNow;
     }
 }
