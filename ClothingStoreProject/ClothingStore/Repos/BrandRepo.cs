347545d1715ed9a54d5e7f988b17fd7b157007af
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-07-22 09:21:08 +0400

    i finished make Brand Repo

--- ClothingStoreProject/ClothingStore/Repos/BrandRepo.cs
+++ ClothingStoreProject/ClothingStore/Repos/BrandRepo.cs
@@ -1,6 +1,53 @@
-﻿namespace ClothingStore.Repos
+﻿using ClothingStore.Models;
+
+
+namespace ClothingStore.Repos
 {
     public class BrandRepo
     {
+        private ClothingStoreContext context;
+
+        public BrandRepo(ClothingStoreContext _context)
+        {
+            context = _context;
+        }
+
+        public List<Brand> GetAll()
+        {
+            return context.brands.ToList();
+        }
+
+        public Brand GetById(int id)
+        {
+            return context.brands.FirstOrDefault(b => b.brandId == id);
+        }
+
+        public Brand GetByName(string name)
+        {
+            return context.brands.FirstOrDefault(b => b.brandName == name);
+        }
+
+        public bool NameExists(string name)
+        {
+            return context.brands.Any(b => b.brandName == name);
+        }
+
+        public void Add(Brand brand)
+        {
+            context.brands.Add(brand);
+            context.SaveChanges();
+        }
+
+        public void Update()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(Brand brand)
+        {
+            context.brands.Remove(brand);
+            context.SaveChanges();
+
+        }
     }
 }
