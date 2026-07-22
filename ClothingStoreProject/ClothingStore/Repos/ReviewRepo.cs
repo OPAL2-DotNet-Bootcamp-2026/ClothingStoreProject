3c8839b8d7a243dc9d6603cd65599e61cabbc85e
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-22 09:32:54 +0400

    Create The repo file for the ReviewRepo

--- ClothingStoreProject/ClothingStore/Repos/ReviewRepo.cs
+++ ClothingStoreProject/ClothingStore/Repos/ReviewRepo.cs
@@ -1,6 +1,59 @@
-﻿namespace ClothingStore.Repos
+﻿using ClothingStore.Models;
+
+namespace ClothingStore.Repos
 {
     public class ReviewRepo
     {
-    }
+
+        private ClothingStoreContext context;
+
+        public ReviewRepo(ClothingStoreContext context)
+        {
+            this.context = context;
+        }
+
+
+        public List<Review> GetAllReview()
+        {
+            return context.reviews.ToList();
+        }
+
+        public Review GetReviewById(int id)
+        {
+            return context.reviews.FirstOrDefault(r => r.reviewId == id);
+        }
+
+        public void AddReview(Review review)
+        {
+            context.reviews.Add(review);
+            context.SaveChanges();
+        }
+
+        public void UpdateReview(Review review)
+        {
+            context.reviews.Update(review);
+            context.SaveChanges();
+        }
+
+        public void DeleteReview(int id)
+        {
+            var review = context.reviews.FirstOrDefault(r => r.reviewId == id);
+
+            if (review != null)
+            {
+                context.reviews.Remove(review);
+                context.SaveChanges();
+
+
+            }
+
+
+
+
+
+
+
+        }
+
+      }
 }
