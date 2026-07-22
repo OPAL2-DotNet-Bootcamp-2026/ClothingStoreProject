1c2913051597bf8ae0c8ba9066fe00dad93465c8
MrSn001 <shaheen36.saa@gmail.com>
2026-07-22 11:35:43 +0400

    Implementing Constructor Dependency Injection

--- ClothingStoreProject/ClothingStore/Repos/ProductRepo.cs
+++ ClothingStoreProject/ClothingStore/Repos/ProductRepo.cs
@@ -1,6 +1,15 @@
-﻿namespace ClothingStore.Repos
+﻿using ClothingStore.Models;
+using static ClothingStore.Enums;
+
+namespace ClothingStore.Repos
 {
     public class ProductRepo
     {
+        private ClothingStoreContext context;
+
+        public ProductRepo(ClothingStoreContext _context)
+        {
+            context = _context;
+        }
     }
 }
