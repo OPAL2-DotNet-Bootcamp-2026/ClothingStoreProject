35466226ecb9fe2e50b8e875ffbefef47d113c24
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-23 08:35:56 +0400

    save change

--- ClothingStoreProject/ClothingStore/Services/ReviewService.cs
+++ ClothingStoreProject/ClothingStore/Services/ReviewService.cs
@@ -1,6 +1,61 @@
-﻿namespace ClothingStore.Services
+﻿using ClothingStore.Models;
+using ClothingStore.Repos;
+
+namespace ClothingStore.Services
 {
     public class ReviewService
     {
+        private ReviewRepo repo;
+
+        public ReviewService(ReviewRepo repo)
+
+        {
+            this.repo=repo;
+        }
+
+
+        public List<Review> GetAllReview()
+        {
+            return repo.GetAllReview();
+        }
+
+        public Review GetReviewById(int id)
+        {
+            return repo.GetReviewById(id);
+        }
+
+        public void AddReview(Review review)
+        {
+            repo.AddReview(review);
+        }
+
+        public void UpdateReview(Review review)
+        {
+            repo.UpdateReview(review);
+        }
+
+        public void DeleteReview(int id)
+        {
+            repo.DeleteReview(id);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
