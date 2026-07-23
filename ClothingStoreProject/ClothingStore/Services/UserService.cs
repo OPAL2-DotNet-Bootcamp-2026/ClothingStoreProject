cf723d10a7de945bf645c096bfca5740e2195383
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-23 09:36:41 +0400

    SAVE CHANGE

--- ClothingStoreProject/ClothingStore/Services/UserService.cs
+++ ClothingStoreProject/ClothingStore/Services/UserService.cs
@@ -1,6 +1,82 @@
-﻿namespace ClothingStore.Services
+﻿using ClothingStore.Models;
+using ClothingStore.Repos;
+
+namespace ClothingStore.Services
 {
     public class UserService
     {
+
+        private UserRepo repo;
+
+        public UserService (UserRepo repo)
+        {
+            this.repo = repo;
+        }
+
+
+        // Get All Users
+        public List<User> GetAllUser()
+        {
+            return repo.GetAllUser();
+        }
+
+        // Get User By Id
+        public User GetUserById(int id)
+        {
+            return repo.GetUserById(id);
+        }
+
+        // Get User By Username
+        public User GetUserByUsername(string username)
+        {
+            return repos.GetUserByUsername(username);
+        }
+
+        // Get User By Email
+        public User GetUserByEmail(string email)
+        {
+            return repo.GetUserByEmail(email);
+        }
+
+        // Add User
+        public void RegisterUser(User user)
+        {
+            repo.AddUser(user);
+        }
+
+        // Update User
+        public void Update(User user)
+        {
+            repo.Update(user);
+        }
+
+        // Delete User
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
