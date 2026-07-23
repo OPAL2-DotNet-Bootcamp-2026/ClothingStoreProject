7a82f0d738ae13e7761799c5f5e42c34341d5826
MrSn001 <shaheen36.saa@gmail.com>
2026-07-23 11:50:41 +0400

    Implementing Constructor Dependency Injection

--- ClothingStoreProject/ClothingStore/Repos/ProductVariantRepo.cs
+++ ClothingStoreProject/ClothingStore/Repos/ProductVariantRepo.cs
@@ -2,5 +2,11 @@
 {
     public class ProductVariantRepo
     {
+        private ClothingStoreContext context;
+
+        public ProductVariantRepo(ClothingStoreContext _context)
+        {
+            context = _context;
+        }
     }
 }
