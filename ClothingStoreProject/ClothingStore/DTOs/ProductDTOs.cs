0ea0152d40abd195bccffcad4471cfad0a9ff35d
MrSn001 <shaheen36.saa@gmail.com>
2026-07-23 15:55:35 +0400

    Implemeinting the UpdateProductDto Class

--- ClothingStoreProject/ClothingStore/DTOs/ProductDTOs.cs
+++ ClothingStoreProject/ClothingStore/DTOs/ProductDTOs.cs
@@ -36,4 +36,29 @@ namespace ClothingStore.DTOs
         [MaxLength(300, ErrorMessage = "Care Instructions Can't be more the 300 Characters")]
         public string? careInstructions { get; set; }
     }
+
+    public class UpdateProductDto
+    {
+        [MaxLength(150, ErrorMessage = "Product Name Can't be more the 150 Characters")]
+        public string? productName { get; set; }
+
+        [MaxLength(1000, ErrorMessage = "Description Can't be more the 1000 Characters")]
+        public string? description { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Base price must be greater than 0.")]
+        public decimal? basePrice { get; set; }
+
+        public int? BrandId { get; set; }
+        public int? CategoryId { get; set; }
+        public Gender? gender { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Material Can't be more the 100 Characters")]
+        public string? material { get; set; }
+
+        public ClothingStyle? clothingStyle { get; set; }
+        public Season? season { get; set; }
+
+        [MaxLength(300, ErrorMessage = "Care Instructions Can't be more the 300 Characters")]
+        public string? careInstructions { get; set; }
+    }
 }
