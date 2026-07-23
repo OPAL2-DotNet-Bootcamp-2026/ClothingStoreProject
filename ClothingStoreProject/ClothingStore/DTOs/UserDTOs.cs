d13c711c8b3f1eb0813a17308025e24549767f1b
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-23 15:43:28 +0400

    modfiy the UserDTOs File

--- ClothingStoreProject/ClothingStore/DTOs/UserDTOs.cs
+++ ClothingStoreProject/ClothingStore/DTOs/UserDTOs.cs
@@ -10,23 +10,23 @@ namespace ClothingStore.DTOs
 
         public class UserResponseDto
         {
-            public int UserId { get; set; }
+            public int userId { get; set; }
 
-            public string Username { get; set; }
+            public string userName { get; set; }
 
-            public string Email { get; set; }
+            public string email { get; set; }
 
-            public string FullName { get; set; }
+            public string fullName { get; set; }
 
-            public string? PhoneNumber { get; set; }
+            public string? phoneNumber { get; set; }
 
-            public string? Address { get; set; }
+            public string? address { get; set; }
 
-            public DateTime RegistrationDate { get; set; }
+            public DateTime registrationDate { get; set; }
 
-            public bool IsActive { get; set; }
+            public bool isActive { get; set; }
 
-            public string Role { get; set; }
+            public string role { get; set; }
         }
 
 
@@ -35,27 +35,36 @@ namespace ClothingStore.DTOs
 
         public class RegisterUserDto
         {
-            [Required]
-            [MaxLength(50)]
+     
             public string userName { get; set; }
 
-            [Required]
-            [EmailAddress]
-            [MaxLength(150)]
+
+            [Required(ErrorMessage = "Email is required.")]
+            [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+            [MaxLength(150,ErrorMessage = "Email cannot exceed 150 characters.")]
             public string email { get; set; }
 
-            [Required]
-            [MaxLength(100)]
-            public string passwordHash { get; set; }
 
-            [Required]
-            [MaxLength(100)]
+
+
+            [Required(ErrorMessage = "Password is required.")]
+            [RegularExpression(
+            @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$",
+            ErrorMessage = "Password must be at least 8 characters long and contain uppercase, lowercase, number and special character.")]
+            public string password{ get; set; }
+
+
+
+            [Required(ErrorMessage = "Full name can't be empty!!")]
+            [MaxLength(100,ErrorMessage = "Full name can't be more than 100 characters!!")]
             public string fullName { get; set; }
 
-            [MaxLength(20)]
+
+            [RegularExpression(@"^[79]\d{7}$",ErrorMessage = "Please enter a valid Omani phone number.")]
             public string? phoneNumber { get; set; }
 
-            [MaxLength(300)]
+
+            [MaxLength(300,ErrorMessage = "Address can't be more than 300 characters!!")]
             public string? address { get; set; }
         }
 
@@ -65,11 +74,12 @@ namespace ClothingStore.DTOs
 
         public class LoginDto
         {
-            [Required]
+            [Required(ErrorMessage = "Email is required.")]
+            [EmailAddress(ErrorMessage = "Invalid email format.")]
             public string email { get; set; }
 
-            [Required]
-            public string passwordHash { get; set; }
+            [Required(ErrorMessage = "Password is required.")]
+            public string password{ get; set; }
         }
 
 
@@ -78,25 +88,34 @@ namespace ClothingStore.DTOs
 
         public class UpdateUserDto
         {
-            [Required]
-            [MaxLength(50)]
+           
+            [Required(ErrorMessage = "Username is required.")]
+            [MaxLength(50, ErrorMessage = "Username cannot exceed 50 characters.")]
             public string userName { get; set; }
 
-            [Required]
-            [EmailAddress]
-            [MaxLength(150)]
+
+            [Required(ErrorMessage = "Email is required.")]
+            [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+            [MaxLength(150, ErrorMessage = "Email cannot exceed 150 characters.")]
             public string email { get; set; }
 
-            [Required]
-            [MaxLength(100)]
+
+
+            [Required(ErrorMessage = "Full name can't be empty!!")]
+            [MaxLength(100, ErrorMessage = "Full name can't be more than 100 characters!!")]
             public string fullName { get; set; }
 
-            [MaxLength(20)]
+
+            [RegularExpression(@"^[79]\d{7}$", ErrorMessage = "Please enter a valid Omani phone number.")]
             public string? phoneNumber { get; set; }
 
-            [MaxLength(300)]
+
+
+            [MaxLength(300, ErrorMessage = "Address can't be more than 300 characters!!")]
             public string? address { get; set; }
         }
+
+
 
 
 
@@ -104,16 +123,21 @@ namespace ClothingStore.DTOs
 
         public class ChangePasswordDto
         {
-            [Required]
-            public string CurrentPassword { get; set; }
+            [Required(ErrorMessage = "Current password is requied")]
+            public string currentPassword { get; set; }
 
-            [Required]
-            [MaxLength(100)]
-            public string NewPassword { get; set; }
 
-            [Required]
-            [Compare("NewPassword")]
-            public string ConfirmPassword { get; set; }
+            [Required(ErrorMessage = "New password is required.")]
+            [RegularExpression(
+             @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$",
+            ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number, one special character, and be at least 8 characters long.")]
+            public string newPassword { get; set; }
+
+
+
+            [Required(ErrorMessage = "Please confirm your new password.")]
+            [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+            public string confirmPassword { get; set; }
         }
 
 
