780a33b06df82b15dd04f68d6c68bd09d058f454
Ahmed <alshukairi1234@gmail.com>
2026-07-24 19:07:03 +0400

    feat- created the cartrItemepo.cs.cs and finished setting it up

--- ClothingStoreProject/ClothingStore/Repos/CartItemRepo.cs
+++ ClothingStoreProject/ClothingStore/Repos/CartItemRepo.cs
@@ -1,6 +1,49 @@
-﻿namespace ClothingStore.Repos
+﻿using ClothingStore.Models;
+
+namespace ClothingStore.Repos
 {
     public class CartItemRepo
     {
+        private ClothingStoreContext context;
+
+        public CartItemRepo(ClothingStoreContext _context)
+        {
+            context = _context;
+        }
+
+        public CartItem GetById(int id)
+        {
+            return context.CartItems.FirstOrDefault(ci => ci.cartItemId == id);
+        }
+
+        public CartItem GetByCartAndVariant(int cartId, int variantId)
+        {
+            return context.CartItems
+                .FirstOrDefault(ci => ci.cartId == cartId && ci.variantId == variantId);
+        }
+
+        public void Add(CartItem cartItem)
+        {
+            context.CartItems.Add(cartItem);
+            context.SaveChanges();
+        }
+
+        public void Update()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(CartItem cartItem)
+        {
+            context.CartItems.Remove(cartItem);
+            context.SaveChanges();
+        }
+
+        public void ClearCart(int cartId)
+        {
+            var items = context.CartItems.Where(ci => ci.cartId == cartId);
+            context.CartItems.RemoveRange(items);
+            context.SaveChanges();
+        }
     }
 }
