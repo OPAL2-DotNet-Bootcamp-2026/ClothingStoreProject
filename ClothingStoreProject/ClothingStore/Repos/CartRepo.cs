1ba8fc13e6944490dbb7f41b99093d7eeaa19215
Ahmed <alshukairi1234@gmail.com>
2026-07-24 18:59:35 +0400

    feat- created the cartrepo.cs.cs and finished setting it up

--- ClothingStoreProject/ClothingStore/Repos/CartRepo.cs
+++ ClothingStoreProject/ClothingStore/Repos/CartRepo.cs
@@ -1,6 +1,41 @@
-﻿namespace ClothingStore.Repos
+﻿using ClothingStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClothingStore.Repos
 {
     public class CartRepo
     {
+        private ClothingStoreContext context;
+
+        public CartRepo(ClothingStoreContext _context)
+        {
+            context = _context;
+        }
+
+        public Cart GetById(int id)
+        {
+            return context.Carts
+                .Include(c => c.CartItems)
+                .FirstOrDefault(c => c.cartId == id);
+        }
+
+        public Cart GetByUserId(int userId)
+        {
+            return context.Carts
+                .Include(c => c.CartItems)
+                .FirstOrDefault(c => c.userId == userId);
+        }
+
+        public Cart Add(Cart cart)
+        {
+            context.Carts.Add(cart);
+            context.SaveChanges();
+            return cart;
+        }
+
+        public void Update()
+        {
+            context.SaveChanges();
+        }
     }
 }
