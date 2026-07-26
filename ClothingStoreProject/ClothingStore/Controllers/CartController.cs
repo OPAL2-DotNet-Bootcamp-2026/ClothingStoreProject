8227fc52465efc3d983cd5cfeacc82efc2fc0600
Ahmed <alshukairi1234@gmail.com>
2026-07-26 16:26:20 +0400

    feat- get userId function

--- ClothingStoreProject/ClothingStore/Controllers/CartController.cs
+++ ClothingStoreProject/ClothingStore/Controllers/CartController.cs
@@ -4,7 +4,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ClothingStore.Controllers
 {
-    public class CartController
+    [Route("cart")]
+    [ApiController]
+    public class CartController : ControllerBase
     {
+        private CartService service;
+
+        public CartController(CartService _service)
+        {
+            service = _service;
+        }
+        
+        // TODO: replace [FromQuery] int userId with User.FindFirst("userId") once
+        // JWT auth middleware is wired into Program.cs. Left as a query param for now
+        // so the endpoint is testable in Swagger before auth exists.
+        
+        [HttpGet("{userId}")]
+        public IActionResult GetCart([FromRoute] int userId)
+        {
+            var cart = service.GetByUserId(userId);
+            return Ok(cart);
+        }
+        
     }
 }
