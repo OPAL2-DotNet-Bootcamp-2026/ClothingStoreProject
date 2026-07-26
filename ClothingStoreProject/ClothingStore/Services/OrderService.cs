765232491bc2e035f05f278196cc61cefd2f08da
Ahmed <alshukairi1234@gmail.com>
2026-07-26 15:01:50 +0400

    feat-  setting up the OrderService.cs

--- ClothingStoreProject/ClothingStore/Services/OrderService.cs
+++ ClothingStoreProject/ClothingStore/Services/OrderService.cs
@@ -1,6 +1,19 @@
-﻿namespace ClothingStore.Services
+﻿using ClothingStore.DTOs;
+using ClothingStore.Models;
+using ClothingStore.Repos;
+using static ClothingStore.Enums;
+
+namespace ClothingStore.Services
+
+
 {
     public class OrderService
     {
+        private OrderRepo orderRepo;
+        private OrderItemRepo orderItemRepo;
+        private CartRepo cartRepo;
+        private CartItemRepo cartItemRepo;
+        private ProductVariantRepo variantRepo;
+        
     }
 }
