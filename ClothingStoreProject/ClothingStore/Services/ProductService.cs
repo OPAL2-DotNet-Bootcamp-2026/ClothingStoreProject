79892c3729f2ca1195cc249d25da15ebf43b0b2b
MrSn001 <shaheen36.saa@gmail.com>
2026-07-26 15:18:48 +0400

    Implementing the dependency injection

--- ClothingStoreProject/ClothingStore/Services/ProductService.cs
+++ ClothingStoreProject/ClothingStore/Services/ProductService.cs
@@ -1,6 +1,21 @@
-﻿namespace ClothingStore.Services
+﻿using ClothingStore.Repos;
+
+namespace ClothingStore.Services
 {
     public class ProductService
     {
+        private readonly ProductRepo productRepo;
+        private readonly BrandRepo brandRepo;
+        private readonly CategoryRepo categoryRepo;
+
+        public ProductService(
+            ProductRepo productRepo,
+            BrandRepo brandRepo,
+            CategoryRepo categoryRepo)
+        {
+            this.productRepo = productRepo;
+            this.brandRepo = brandRepo;
+            this.categoryRepo = categoryRepo;
+        }
     }
 }
