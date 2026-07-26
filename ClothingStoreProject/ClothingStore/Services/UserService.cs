efae3319a66bbecb97e20da0b8ff54f1a8a9c8e6
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-26 09:43:38 +0400

    save

--- ClothingStoreProject/ClothingStore/Services/UserService.cs
+++ ClothingStoreProject/ClothingStore/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using ClothingStore.Models;
+﻿using ClothingStore.DTOs;
+using ClothingStore.Models;
 using ClothingStore.Repos;
 
 namespace ClothingStore.Services
@@ -14,7 +15,25 @@ namespace ClothingStore.Services
         }
 
 
+        // Get All Users
+        public List<UserDTOs> GetAllUsers()
+        {
+            return repo.GetAllUsers()
 
+                .Select(u => new UserResponseDto
+                {
+                    userId = u.UserId,
+                    username = u.userName,
+                    email = u.email,
+                    fullName = u.fullName,
+                    phoneNumber = u.phoneNumber,
+                    address = u.address,
+                    registrationDate = u.registrationDate,
+                    Isactive = u.isActive,
+                    role = u.role
+                })
+                .ToList();
+        }
 
 
 
