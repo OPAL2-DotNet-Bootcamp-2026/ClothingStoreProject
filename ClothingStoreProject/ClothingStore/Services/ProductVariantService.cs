29c3fcfcf172b56c6667050817dff2f2fc99a088
MrSn001 <shaheen36.saa@gmail.com>
2026-07-26 18:39:32 +0400

    Implementing Dependency Injection

--- ClothingStoreProject/ClothingStore/Services/ProductVariantService.cs
+++ ClothingStoreProject/ClothingStore/Services/ProductVariantService.cs
@@ -1,6 +1,18 @@
-﻿namespace ClothingStore.Services
+﻿using ClothingStore.Repos;
+
+namespace ClothingStore.Services
 {
     public class ProductVariantService
     {
+        private ProductVariantRepo productVariantRepo;
+        private ProductRepo productRepo;
+
+        public ProductVariantService(
+            ProductVariantRepo _productVariantRepo,
+            ProductRepo _productRepo)
+        {
+            productVariantRepo = _productVariantRepo;
+            productRepo = _productRepo;
+        }
     }
 }
