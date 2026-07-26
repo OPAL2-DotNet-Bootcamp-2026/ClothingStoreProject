bc950a15c3c5015512d3b1fd38c60bf2774bceaa
Ahmed <alshukairi1234@gmail.com>
2026-07-26 10:50:43 +0400

    created the CartService.cs and the finished setting it up

--- ClothingStoreProject/ClothingStore/Services/CartService.cs
+++ ClothingStoreProject/ClothingStore/Services/CartService.cs
@@ -1,6 +1,28 @@
-﻿namespace ClothingStore.Services
+﻿using ClothingStore.DTOs;
+using ClothingStore.Models;
+using ClothingStore.Repos;
+
+namespace ClothingStore.Services
 {
     public class CartService
     {
+        private CartRepo cartRepo;
+        private CartItemRepo cartItemRepo;
+        private ProductVariantRepo variantRepo;
+
+        public CartService(CartRepo _cartRepo, CartItemRepo _cartItemRepo, ProductVariantRepo _variantRepo)
+        {
+            cartRepo = _cartRepo;
+            cartItemRepo = _cartItemRepo;
+            variantRepo = _variantRepo;
+        }
+
+    
+
+
+
+
+
+
     }
 }
