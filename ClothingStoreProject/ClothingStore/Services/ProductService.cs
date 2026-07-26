a3fdbbdd9ae23cc664391d2219fa90812dec4b65
MrSn001 <shaheen36.saa@gmail.com>
2026-07-26 15:20:14 +0400

    Implementing MapToListDto(Product product)

--- ClothingStoreProject/ClothingStore/Services/ProductService.cs
+++ ClothingStoreProject/ClothingStore/Services/ProductService.cs
@@ -1,4 +1,6 @@
-﻿using ClothingStore.Repos;
+﻿using ClothingStore.DTOs;
+using ClothingStore.Models;
+using ClothingStore.Repos;
 
 namespace ClothingStore.Services
 {
@@ -16,6 +18,22 @@ namespace ClothingStore.Services
             this.productRepo = productRepo;
             this.brandRepo = brandRepo;
             this.categoryRepo = categoryRepo;
+        }
+
+
+        private ProductListItemDto MapToListDto(Product product)
+        {
+            return new ProductListItemDto
+            {
+                productId = product.productId,
+                productName = product.productName,
+                basePrice = product.basePrice,
+                BrandName = product.Brand?.brandName ?? "Unknown",
+                CategoryName =
+                    product.Category?.categoryName ?? "Unknown",
+                gender = product.gender,
+                isAvailable = product.isAvailable
+            };
         }
     }
 }
