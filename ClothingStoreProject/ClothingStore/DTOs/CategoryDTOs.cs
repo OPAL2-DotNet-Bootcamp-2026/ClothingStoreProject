bbdcae50d4d3ad590c8dcf95f4ff7ffde2562b4e
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-07-27 08:50:46 +0400

    declear vereble for CategoryDTOs

--- ClothingStoreProject/ClothingStore/DTOs/CategoryDTOs.cs
+++ ClothingStoreProject/ClothingStore/DTOs/CategoryDTOs.cs
@@ -2,5 +2,11 @@
 {
     public class CategoryDTOs
     {
+        public int categoryId {get; set;}
+        public string categoryName {get; set;}
+        public string? description {get; set;}
+        public string? imageUrl {get; set;}
+        public int? parentCategoryId {get; set;}
+    }
     }
 }
