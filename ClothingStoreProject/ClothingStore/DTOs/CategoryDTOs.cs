d0d46b264915127e179e75948786a588db8f3037
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-07-27 08:53:46 +0400

    Added Data annotation for CreateCategoryDto

--- ClothingStoreProject/ClothingStore/DTOs/CategoryDTOs.cs
+++ ClothingStoreProject/ClothingStore/DTOs/CategoryDTOs.cs
@@ -11,8 +11,13 @@
 
     public class CreateCategoryDto
     {
+        [Required(ErrorMessage = "category Name Can't be Empty!!")]
+        [MaxLength(100, ErrorMessage = "category Name Can't be more than 100 Character")]
         public string categoryName {get; set;}
+        [MaxLength(500, ErrorMessage = "description Can't be more than 500 Character")]
         public string? description {get; set;}
+
+        [MaxLength(300, ErrorMessage = "image Url Can't be more than 300 Character")]
         public string? imageUrl {get; set;}
         public int? parentCategoryId {get; set;}
     }
