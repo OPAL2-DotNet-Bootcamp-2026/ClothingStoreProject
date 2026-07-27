15831bb32e140eb6d0e1e1f2527da7af94ce816d
Ahmed <alshukairi1234@gmail.com>
2026-07-27 09:12:52 +0400

    feat- finish the setup of the OrderController.cs

--- ClothingStoreProject/ClothingStore/Controllers/OrderController.cs
+++ ClothingStoreProject/ClothingStore/Controllers/OrderController.cs
@@ -1,5 +1,8 @@
-﻿namespace ClothingStore.Controllers
-{
+﻿using ClothingStore.DTOs;
+using ClothingStore.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClothingStore.Controllers{
     public class OrderController
     {
     }
