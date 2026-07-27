f4f42889c717db376f7e7dc26811d5130181b0df
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-27 12:36:26 +0400

    implement user_controller

--- ClothingStoreProject/ClothingStore/Controllers/UserController.cs
+++ ClothingStoreProject/ClothingStore/Controllers/UserController.cs
@@ -4,9 +4,7 @@ using static ClothingStore.DTOs.UserDTOs;
 
 namespace ClothingStore.Controllers
 {
-    public class UserController
-    {
-
+ 
         [ApiController]
         [Route("user")]
         public class UserController : ControllerBase
@@ -42,20 +40,27 @@ namespace ClothingStore.Controllers
 
             {
 
-                UserResponseDto user = userService.GetUserById(id);
+                UserResponseDto ? user = userService.GetUserById(id);
+
+                if (user == null)
+                {
+                    return NotFound("user not found ");
+                }
 
                 return Ok(user);
 
             }
 
+
+
+
             // GET: user/GetUsersByRole?role=Customer
 
             [HttpGet("GetUsersByRole")]
 
-            public IActionResult GetUsersByRole([FromQuery] string role)
+            public IActionResult GetUsersByRole([FromQuery] Enums.Role role)
 
             {
-
                 List<UserResponseDto> users = userService.GetUsersByRole(role);
 
                 return Ok(users);
@@ -65,41 +70,130 @@ namespace ClothingStore.Controllers
 
 
 
+            // POST: user/RegisterUser
+
+            [HttpPost("RegisterUser")]
+
+            public IActionResult RegisterUser([FromBody] RegisterUserDto dto)
+
+            {
+
+                UserResponseDto ? user = userService.RegisterUser(dto);
+
+                if (user == null)
+                {
+                    return BadRequest("Email or Username already exists.");
+
+                }
+                return Ok(user);
+
+            }
 
 
 
 
 
+            // POST: user/LoginUser
+
+            [HttpPost("LoginUser")]
+
+            public IActionResult LoginUser([FromBody] LoginDto dto)
+
+            {
+                UserResponseDto? user = userService.LoginUser(dto);
+                 
+                if(user == null)
+                { 
+                    return BadRequest("Invalid email, password ,or inactive account.");
+                }
+
+                return Ok(user);
+
+            }
+
+
+
+            // PUT: user/UpdateUserProfile?id=1
+            [HttpPut("UpdateUserProfile")]
+
+            public IActionResult UpdateUserProfile( [FromQuery] int id, [FromBody] UpdateUserDto dto)
+            {
+                UserResponseDto? user = userService.UpdateUserProfile(id, dto);
+
+                if (user == null)
+                {
+                    return NotFound("User not found.");
+                }
+
+                return Ok(user);
+            }
 
 
 
 
+            // PUT: user/ChangeUserPassword?id=1
+            [HttpPut("ChangeUserPassword")]
+              public IActionResult ChangeUserPassword([FromQuery] int id, [FromBody] ChangePasswordDto dto)
+                 {
+                bool changed = userService.ChangeUserPassword(id, dto);
+
+                if (!changed)
+                {
+                    return BadRequest("Password change failed.");
+                }
+
+                return Ok("Password changed successfully.");
+                 }
 
 
 
 
+         
+            
+            // PATCH: user/SetUserActiveStatus?id=1
+            [HttpPatch("SetUserActiveStatus")]
 
+            public IActionResult SetUserActiveStatus( [FromQuery] int id, [FromBody] SetActiveStatusDto dto)
+            {
+                bool updated = userService.SetUserActiveStatus(id, dto);
 
+                if (!updated)
+                {
+                    return NotFound("User not found.");
+                }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                return Ok("User status updated successfully.");
+            }
         }
-
-
     }
 
 
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
