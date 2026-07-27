b15c6ac43794094891285a285687be765ed6059f
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-07-27 09:56:01 +0400

    Added brand DTO

--- ClothingStoreProject/ClothingStore/DTOs/BrandDTOs.cs
+++ ClothingStoreProject/ClothingStore/DTOs/BrandDTOs.cs
@@ -1,6 +1,48 @@
-﻿namespace ClothingStore.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClothingStore.DTOs
 {
     public class BrandDTOs
     {
+        public class BrandResponseDto
+        {
+            public int brandId { get; set; }
+            public string brandName { get; set; }
+            public string? description { get; set; }
+            public string? logoUrl { get; set; }
+            public string? websiteUrl { get; set; }
+            public bool isActive { get; set; }
+        }
+
+        public class CreateBrandDto
+        {
+            [Required(ErrorMessage = "Brand Name Can't be Empty!!")]
+            [MaxLength(100, ErrorMessage = "Brand Name Can't be more than 100 Character")]
+            public string brandName { get; set; }
+
+            [MaxLength(500, ErrorMessage = "description Can't be more than 500 Character")]
+            public string? description { get; set; }
+
+            [MaxLength(300, ErrorMessage = "logo Url Can't be more than 100 Character")]
+            public string? logoUrl { get; set; }
+
+            [MaxLength(200, ErrorMessage = "website Url Can't be more than 100 Character")]
+            public string? websiteUrl { get; set; }
+        }
+
+        public class UpdateBrandDto
+        {
+            [MaxLength(100, ErrorMessage = "Brand Name Can't be more than 100 Character")]
+            public string? brandName { get; set; }
+
+            [MaxLength(500, ErrorMessage = "description Can't be more than 500 Character")]
+            public string? description { get; set; }
+
+            [MaxLength(300, ErrorMessage = "logo Url Can't be more than 100 Character")]
+            public string? logoUrl { get; set; }
+
+            [MaxLength(200, ErrorMessage = "website Url Can't be more than 100 Character")]
+            public string? websiteUrl { get; set; }
+        }
     }
 }
