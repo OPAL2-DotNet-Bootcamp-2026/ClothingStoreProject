fbd00d97aa3ee131be8ab853b430584c51e38e18
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-07-27 08:54:59 +0400

    Added All verable with Data

--- ClothingStoreProject/ClothingStore/DTOs/CategoryDTOs.cs
+++ ClothingStoreProject/ClothingStore/DTOs/CategoryDTOs.cs
@@ -1,4 +1,6 @@
-﻿namespace ClothingStore.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClothingStore.DTOs
 {
     public class CategoryDTOs
     {
@@ -14,13 +16,28 @@
         [Required(ErrorMessage = "category Name Can't be Empty!!")]
         [MaxLength(100, ErrorMessage = "category Name Can't be more than 100 Character")]
         public string categoryName {get; set;}
+
         [MaxLength(500, ErrorMessage = "description Can't be more than 500 Character")]
         public string? description {get; set;}
 
         [MaxLength(300, ErrorMessage = "image Url Can't be more than 300 Character")]
         public string? imageUrl {get; set;}
+
         public int? parentCategoryId {get; set;}
     }
+
+    public class UpdateCategoryDto
+    {
+        [MaxLength(100, ErrorMessage = "category Name Can't be more than 100 Character")]
+        public string? categoryName {get; set;}
+
+        [MaxLength(500, ErrorMessage = "description Can't be more than 500 Character")]
         public string? description {get; set;}
+
+        [MaxLength(300, ErrorMessage = "image Url Can't be more than 300 Character")]
+        public string? imageUrl {get; set;}
+
+        public int? parentCategoryId {get; set;}
+
     }
 }
