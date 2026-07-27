188768b6a32827adcc25d52703fe2b528cb99ef1
MrSn001 <shaheen36.saa@gmail.com>
2026-07-27 15:50:54 +0400

    Implementing Dependency Injection

--- ClothingStoreProject/ClothingStore/Controllers/ProductController.cs
+++ ClothingStoreProject/ClothingStore/Controllers/ProductController.cs
@@ -1,6 +1,19 @@
-﻿namespace ClothingStore.Controllers
+﻿using ClothingStore.Services;
+using Microsoft.AspNetCore.Mvc;
+using static ClothingStore.Enums;
+
+namespace ClothingStore.Controllers
 {
-    public class ProductController
+    [ApiController]
+    [Route("product")]
+    public class ProductController : ControllerBase
     {
+        private readonly ProductService productService;
+
+        public ProductController(ProductService productService)
+        {
+            this.productService = productService;
+        }
+
     }
 }
