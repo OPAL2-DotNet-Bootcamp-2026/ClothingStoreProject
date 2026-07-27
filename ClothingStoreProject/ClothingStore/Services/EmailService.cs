29bdc157d5b5e9bf65a66df5252a6261e1424ce1
Ahmed <alshukairi1234@gmail.com>
2026-07-27 15:54:57 +0400

    feat - email service done

--- ClothingStoreProject/ClothingStore/Services/EmailService.cs
+++ ClothingStoreProject/ClothingStore/Services/EmailService.cs
@@ -13,5 +13,19 @@ public class EmailService : IEmailService
     {
         _config = config;
     }
+    public async Task SendAsync(string toEmail, string subject, string body)
+    {
+        var message = new MimeMessage();
+        message.From.Add(MailboxAddress.Parse(_config["Email:From"]));
+        message.To.Add(MailboxAddress.Parse(toEmail));
+        message.Subject = subject;
+        message.Body = new TextPart("html") { Text = body };
+
+        using var smtp = new SmtpClient();
+        await smtp.ConnectAsync(_config["Email:Host"], int.Parse(_config["Email:Port"]), true);
+        await smtp.AuthenticateAsync(_config["Email:Username"], _config["Email:Password"]);
+        await smtp.SendAsync(message);
+        await smtp.DisconnectAsync(true);
+    }
 
 }
