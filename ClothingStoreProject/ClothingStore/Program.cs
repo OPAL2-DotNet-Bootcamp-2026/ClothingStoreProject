85078dfded5ada24c7b4b97c0ae1329d79df3d2c
MrSn001 <shaheen36.saa@gmail.com>
2026-07-28 12:11:41 +0400

    Add the Register repositories and services

--- ClothingStoreProject/ClothingStore/Program.cs
+++ ClothingStoreProject/ClothingStore/Program.cs
@@ -1,4 +1,6 @@
 
+using ClothingStore.Repos;
+using ClothingStore.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace ClothingStore
@@ -12,7 +14,30 @@ namespace ClothingStore
             builder.Services.AddDbContext<ClothingStoreContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            
+            // Register repositories
+            builder.Services.AddScoped<BrandRepo>();
+            builder.Services.AddScoped<CartItemRepo>();
+            builder.Services.AddScoped<CartRepo>();
+            builder.Services.AddScoped<CategoryRepo>();
+            builder.Services.AddScoped<OrderItemRepo>();
+            builder.Services.AddScoped<OrderRepo>();
+            builder.Services.AddScoped<ProductRepo>();
+            builder.Services.AddScoped<ProductVariantRepo>();
+            builder.Services.AddScoped<ReviewRepo>();
+            builder.Services.AddScoped<UserRepo>();
+
+            //Register Service
+            builder.Services.AddScoped<BrandService>();
+            builder.Services.AddScoped<CartService>();
+            builder.Services.AddScoped<CategoryService>();
+            builder.Services.AddScoped<EmailService>();
+            builder.Services.AddScoped<OrderService>();
+            builder.Services.AddScoped<ProductService>();
+            builder.Services.AddScoped<ProductVariantService>();
+            builder.Services.AddScoped<ReviewService>();
+            builder.Services.AddScoped<UserService>();
+
+
 
 
 
